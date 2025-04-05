874f6f1d3f00330b22f8cfc6926eb88deeae0d15
lawl1ng <199223655+lawl1ng@users.noreply.github.com>
2025-04-05 09:35:48 +0700

    Adding sololearn repo and popsicles challenge To showcase challenges completed

---
+++ easy/popsicles.py
@@ -0,0 +1,11 @@
+siblings = int(input())
+popsicles = int(input())
+
+#your code goes here
+#print(type(siblings))
+#print("int")
+#print(int)
+if ((popsicles/siblings)==int(popsicles/siblings)):
+    print("give away")
+else:
+    print("eat them yourself")
